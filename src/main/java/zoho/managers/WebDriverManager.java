3ada36740bebdca9c6770ac65703a33a1424ce4f
Syedf@izan520 <faizanhussain520@gmail.com>
2023-07-31 15:13:55 -0700

    added some utility methods

--- src/main/java/zoho/managers/WebDriverManager.java
+++ src/main/java/zoho/managers/WebDriverManager.java
@@ -1,6 +1,8 @@
 package zoho.managers;
 
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,8 +16,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class WebDriverManager {
 
     WebDriver driver;
+    ExtentTest test;
+
 
     public void openBrowser(String browser){
+        test.log(Status.INFO, "opening browser");
+        System.setProperty("webdriver.chrome.driver","C:\\Tools\\ZOHOCucumberPOM\\chromedriver.exe");
         if(browser.equals("Mozilla")){
            driver = new FirefoxDriver();
 
@@ -33,8 +39,25 @@ public class WebDriverManager {
         driver.findElement(By.xpath(locatorKey)).click();
     }
 
+    /*************Utility Functions******************/
+
+    public void init(ExtentTest test) {
+        this.test=test;
+    }
 
 
+    public void quit() {
+    }
 
+    public void log(String msg) {
+        System.out.println(msg);
+        test.log(Status.INFO, msg);
+    }
 
+    public boolean isElementPresent(String usernameId) {
+        return false;
+    }
+
+    public void logFailure(String loginPageDidNotLoad, boolean b) {
+    }
 }
